187d643410f80a423e4d494d2e7555913fdede32
James <26018087+Jameschan215@user.noreply.github.com>
2024-11-21 11:35:37 +0800

    4th commit - change color scheme and layout

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ function Constants() {
 
 function Cell() {
 	let value = 0;
+	let inGroup = false;
 
 	const addToken = (token) => {
 		value = token;
@@ -14,7 +15,12 @@ function Cell() {
 
 	const getValue = () => value;
 
-	return { addToken, getValue };
+	const setInGroup = (value) => {
+		inGroup = value;
+	};
+	const getInGroup = () => inGroup;
+
+	return { addToken, getValue, setInGroup, getInGroup };
 }
 
 function GameBoard() {
@@ -51,20 +57,32 @@ function GameBoard() {
 	return { setBoard, getBoard, dropToken, printBoard };
 }
 
-function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
-	const players = [
-		{ name: playerOneName, token: 1, won: false },
-		{ name: playerTwoName, token: 2, won: false },
-	];
-	let activePlayer = players[0];
+function GamePlayer(name, token) {
+	return {
+		name: name,
+		token: token,
+		win: false,
+		count: 0,
+	};
+}
+
+function GameController() {
+	const playerOne = GamePlayer('Player 1', 1);
+	const playerTwo = GamePlayer('Player 2', 2);
+	console.log({ playerOne });
+
+	let activePlayer = playerOne;
+	let tie = false;
 
 	const gameBoard = GameBoard();
 
 	const switchPlayerTurn = () => {
-		activePlayer = activePlayer === players[0] ? players[1] : players[0];
+		activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
 	};
 
 	const getActivePlayer = () => activePlayer;
+
+	const getTie = () => tie;
 
 	const printNewRound = () => {
 		gameBoard.printBoard();
@@ -104,6 +122,7 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 
 				for ([dr, dc] of directions) {
 					let count = 0;
+					let winGroup = [];
 
 					for (let i = 0; i < 3; i++) {
 						const rowIndex = row + dr * i;
@@ -117,9 +136,15 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 							board[rowIndex][colIndex].getValue() === token
 						) {
 							count += 1;
+							winGroup.push(board[rowIndex][colIndex]);
 						}
 					}
-					if (count >= 3) return true;
+					if (count >= 3) {
+						winGroup.forEach((cell) => cell.setInGroup(true));
+						return true;
+					} else {
+						winGroup = [];
+					}
 				}
 			}
 		}
@@ -149,13 +174,17 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 
 		// Check win and tie here
 		if (checkWin(gameBoard.getBoard())) {
-			activePlayer.won = true;
+			activePlayer.win = true;
+			activePlayer.count += 1;
 			gameBoard.printBoard();
-			console.log(`${getActivePlayer().name} won the game!`);
+			console.log(`${getActivePlayer().name} win the game!`);
 			return;
 		} else if (checkTie(gameBoard.getBoard())) {
+			tie = true;
+			console.log({ tie });
+
 			gameBoard.printBoard();
-			console.log("No one won the game. It's a tie!");
+			console.log("No one win the game. It's a tie!");
 			return;
 		}
 
@@ -164,17 +193,30 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 	};
 
 	const resetGame = () => {
+		nextGame();
+		playerOne.count = 0;
+		playerTwo.count = 0;
+	};
+
+	const nextGame = () => {
 		gameBoard.setBoard();
-		activePlayer = players[0];
-		printNewRound();
+
+		activePlayer = playerOne;
+		playerOne.win = false;
+		playerTwo.win = false;
+		tie = false;
 	};
 
 	resetGame();
 
 	return {
+		getTie,
 		playRound,
 		getActivePlayer,
+		playerOne,
+		playerTwo,
 		resetGame,
+		nextGame,
 		getBoard: gameBoard.getBoard,
 	};
 }
@@ -186,8 +228,15 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 	const oIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"/></svg>`;
 
 	const turnInfoDom = document.querySelector('.turn-info');
-	const endInfo = document.querySelector('.end-info');
 	const boardDom = document.querySelector('.board');
+	const resetBtnDom = document.querySelector('.buttons #reset');
+	const nextBtnDom = document.querySelector('.buttons #next');
+	const playerOneName = document.querySelector('#player-1 .name');
+	const playerTwoName = document.querySelector('#player-2 .name');
+	const countOneDom = document.querySelector('#player-1 .count');
+	const countTwoDom = document.querySelector('#player-2 .count');
+	const playerInfoOne = document.querySelector('#player-1');
+	const playerInfoTwo = document.querySelector('#player-2');
 
 	const updateScreen = () => {
 		// Clear board
@@ -197,8 +246,21 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 		const board = game.getBoard();
 		const activePlayer = game.getActivePlayer();
 
-		// Display turn info
-		turnInfoDom.textContent = `${activePlayer.name}'s turn ...`;
+		// Render players information
+		playerOneName.textContent = game.playerOne.name;
+		countOneDom.textContent =
+			game.playerOne.count === 0 ? '-' : game.playerOne.count;
+		playerTwoName.textContent = game.playerTwo.name;
+		countTwoDom.textContent =
+			game.playerTwo.count === 0 ? '-' : game.playerTwo.count;
+
+		if (activePlayer.token === 1) {
+			playerInfoOne.classList.add('active');
+			playerInfoTwo.classList.remove('active');
+		} else {
+			playerInfoOne.classList.remove('active');
+			playerInfoTwo.classList.add('active');
+		}
 
 		// Render board
 		board.forEach((row, rowIndex) => {
@@ -206,7 +268,9 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 				const cellDom = document.createElement('button');
 				cellDom.dataset.row = rowIndex;
 				cellDom.dataset.col = colIndex;
+
 				cellDom.classList.add('cell');
+
 				if (cell.getValue() === 1) {
 					cellDom.classList.add('cellX');
 					cellDom.innerHTML = xIcon;
@@ -215,9 +279,31 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 					cellDom.innerHTML = oIcon;
 				}
 
+				// if cells in line 3
+				if (cell.getInGroup()) {
+					cellDom.classList.add('win-group');
+				}
+
 				boardDom.appendChild(cellDom);
 			});
 		});
+
+		// Display turn info
+		if (activePlayer.win) {
+			turnInfoDom.textContent = `${activePlayer.name} wins!`;
+			turnInfoDom.classList.add('win-info');
+			toggleBoardFrozen();
+		} else if (game.getTie()) {
+			turnInfoDom.textContent = `Tie.`;
+		} else {
+			turnInfoDom.textContent = `${activePlayer.name}'s turn.`;
+		}
+	};
+
+	const toggleBoardFrozen = () => {
+		boardDom
+			.querySelectorAll('button')
+			.forEach((btn) => (btn.disabled = !btn.disabled));
 	};
 
 	function boardClickHandler(e) {
@@ -232,7 +318,81 @@ function GameController(playerOneName = 'Tom', playerTwoName = 'Jerry') {
 		updateScreen();
 	}
 
+	const resetGame = () => {
+		game.resetGame();
+		toggleBoardFrozen();
+		turnInfoDom.classList.remove('win-info');
+		updateScreen();
+	};
+
+	const nextGame = () => {
+		game.nextGame();
+		toggleBoardFrozen();
+		turnInfoDom.classList.remove('win-info');
+		updateScreen();
+	};
+
 	boardDom.addEventListener('click', boardClickHandler);
+	resetBtnDom.addEventListener('click', resetGame);
+	nextBtnDom.addEventListener('click', nextGame);
+
+	playerOneName.addEventListener('keydown', (event) => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+
+			game.playerOne.name = event.target.value;
+			playerOneName.blur();
+			updateScreen();
+		}
+	});
+
+	playerOneName.addEventListener('focusout', (event) => {
+		event.preventDefault();
+
+		game.playerOne.name = event.target.value;
+		playerOneName.blur();
+		updateScreen();
+	});
+
+	playerTwoName.addEventListener('keydown', (event) => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+
+			game.playerTwo.name = event.target.value;
+			playerTwoName.blur();
+
+			updateScreen();
+		}
+	});
+
+	playerTwoName.addEventListener('focusout', (event) => {
+		event.preventDefault();
+
+		game.playerTwo.name = event.target.value;
+		playerOneName.blur();
+		updateScreen();
+	});
 
 	updateScreen();
+	showForm();
 })();
+
+function showForm() {
+	const dialogDom = document.querySelector('dialog');
+	const closeDom = document.querySelector('#close');
+	const formDom = document.querySelector('form');
+
+	dialogDom.showModal();
+
+	formDom.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+		console.log(Object.fromEntries(formData.entries()));
+
+		formDom.reset();
+		dialogDom.close();
+	});
+
+	closeDom.addEventListener('click', () => dialogDom.close());
+}
